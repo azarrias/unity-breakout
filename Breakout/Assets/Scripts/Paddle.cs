28764d3df87f96e23e3f5c9628736168cf36af6c
azarrias <adolfo.zarrias@gmail.com>
2021-10-23 17:31:09 +0200

    Add lose trigger collider that transitions to game over scene and move paddle with mouse x position

---
+++ Breakout/Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    private float screenWidthInUnits;
+
+    void Start()
+    {
+        screenWidthInUnits = Camera.main.aspect * Camera.main.orthographicSize * 2;
+    }
+
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        var paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
