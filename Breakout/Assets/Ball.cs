19bc8d3189c64c092ac840cf0c244bb0ad031f7f
azarrias <adolfo.zarrias@gmail.com>
2021-10-23 18:40:07 +0200

    Add ball script to manage ball starting position, also fix scripts execution order

---
+++ Breakout/Assets/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private Paddle paddle;
+    private Vector2 paddleToBallDistance;
+
+    void Start()
+    {
+        paddleToBallDistance = transform.position - paddle.transform.position;
+    }
+
+    void Update()
+    {
+        var paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleToBallDistance;
+    }
+}
