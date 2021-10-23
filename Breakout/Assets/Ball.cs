cb9d1c259546017e07cd0cc6bfc13f91f2263b34
azarrias <adolfo.zarrias@gmail.com>
2021-10-24 00:44:51 +0200

    Launch ball

--- Breakout/Assets/Ball.cs
+++ Breakout/Assets/Ball.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
     [SerializeField] private Paddle paddle;
+    [SerializeField] private float launchVelocityX = 2f;
+    [SerializeField] private float launchVelocityY = 15f;
     private Vector2 paddleToBallDistance;
+    private bool hasLaunched = false;
 
     void Start()
     {
@@ -13,6 +17,24 @@ public class Ball : MonoBehaviour
     }
 
     void Update()
+    {
+        if (!hasLaunched)
+        {
+            LockToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasLaunched = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(launchVelocityX, launchVelocityY);
+        }
+    }
+
+    private void LockToPaddle()
     {
         var paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
         transform.position = paddlePos + paddleToBallDistance;
