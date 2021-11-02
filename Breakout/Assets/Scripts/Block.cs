7b2a0220a622951f29af49f4582b91e1d220ac84
azarrias <adolfo.zarrias@gmail.com>
2021-11-02 20:26:00 +0100

    Play sound even when blocks are being destroyed

--- Breakout/Assets/Scripts/Block.cs
+++ Breakout/Assets/Scripts/Block.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private AudioClip breakSound;
+
     private void OnCollisionExit2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
