d5a66c20cabde92cd9507858d51b01b7391613b6
azarrias <adolfo.zarrias@gmail.com>
2021-11-03 20:45:11 +0100

    Keep account of the number of destroyed blocks and travel to next level when all are gone

--- Breakout/Assets/Scripts/Level.cs
+++ Breakout/Assets/Scripts/Level.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class Level : MonoBehaviour
 {
     // Serialized only for debugging purposes
-    [SerializeField] private int breakableBlocks; 
+    [SerializeField] private int breakableBlocks;
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
 
     public void IncreaseNumberOfBreakableBlocks()
     {
         breakableBlocks++;
+    }
+
+    public void DecreaseNumberOfBreakableBlocks()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
     }
 }
