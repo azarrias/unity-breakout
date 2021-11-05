622d66fe31bdb2d36fd0257d7ee0af741e518040
azarrias <adolfo.zarrias@gmail.com>
2021-11-05 18:35:30 +0100

    Add GameStatus to keep track of game data and configuration and include all necessary prefabs in the scenes

---
+++ Breakout/Assets/Scripts/GameStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    [Range(0.1f, 10f)]
+    [SerializeField] private float timeScale = 1f;
+
+    void Start()
+    {
+        Time.timeScale = timeScale;
+    }
+
+    void Update()
+    {
+ 
+    }
+}
